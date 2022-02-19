7e4fbeabfb9a9234ba321cb49f5d68d55ed5b056
KTaskn <korai.kinjo@gmail.com>
2022-02-19 23:17:44 +0900

    バージョンアップ

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def _requires_from_file(filename):
 
 setup(
     name='scorewithimage',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=_requires_from_file('requirements.txt'),
     include_package_data=True
