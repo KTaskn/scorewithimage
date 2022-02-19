6f3a98b4cc8c2db4c398aff9968fc5f33b9101f1
KTaskn <korai.kinjo@gmail.com>
2022-02-19 23:15:26 +0900

    requirementsを追加

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
 setup(
     name='scorewithimage',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
+    install_requires=_requires_from_file('requirements.txt'),
     include_package_data=True
 )
